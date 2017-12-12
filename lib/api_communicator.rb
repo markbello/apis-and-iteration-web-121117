f1fd4bad3ba2782291ab2302c178e66921414517
markbello <mcbello51286@gmail.com>
2017-12-12 14:50:22 -0500

    Initial commit

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,13 +2,26 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character)
-  #make the web request
+def character_hash_result
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
-  
-  # iterate over the character hash to find the collection of `films` for the given
-  #   `character`
+end
+
+def get_character_movies_from_api(character)
+  #make the web request
+
+  characters = character_hash_result["results"]
+  found_character = characters.find { |char| char["name"] == character }
+  films = found_character["films"]
+
+
+all_films_json = films.map { |film_url| RestClient.get(film_url) }
+films_parsed = all_films_json.map { |film| JSON.parse(film) }
+
+film_titles = films_parsed.map { |film| film["title"]}
+
+film_titles
+
   # collect those film API urls, make a web request to each URL to get the info
   #  for that film
   # return value of this method should be collection of info about each film.
@@ -19,7 +32,7 @@ def get_character_movies_from_api(character)
 end
 
 def parse_character_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
+  films_hash.each { |film| puts "- #{film}" }
 end
 
 def show_character_movies(character)
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,14 @@
 def welcome
-  # puts out a welcome message here!
+  puts "Hello!"
 end
 
 def get_character_from_user
   puts "please enter a character"
   # use gets to capture the user's input. This method should return that input, downcased.
+  input = gets.chomp
+  input.downcase!
+  input_array = input.split
+  formatted_name_array = input_array.map{|word| word.capitalize}
+  formatted_name = formatted_name_array.join(" ")
+  formatted_name
 end
